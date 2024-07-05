2d831fd3ac16bcb50a08eb8900a2c9bd8f0ed6e8
Brijesh Patel <brijeshpatel96@gmail.com>
2024-07-05 14:12:23 +0100

    adding Hero and Nav

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Destinations from './components/Destinations'
+
+function App() {
+  return (
+    <div>
+        <Navbar />
+        <Hero />
+        <Destinations />
+    </div>
+  )
+}
+
+export default App
